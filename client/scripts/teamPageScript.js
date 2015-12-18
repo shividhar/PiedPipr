81a3472e1b402a80c030bc7a2b0065fa61414ca1
sum-kcid <jayp1924@gmail.com>
2015-12-18 03:39:46 -0500

    team page animation updates

--- client/scripts/teamPageScript.js
+++ client/scripts/teamPageScript.js
@@ -5,15 +5,25 @@ if (Meteor.isClient) {
   		$('#footer').show();
   		executeResizeFuncs();
 
-  		$('.demo td').each(function(index, el) {
-  			var $this = $(el);
-  			setTimeout(function() {
-  				$this.css({
-  					'opacity': '1',
-  					'top': '0'
-  				});
-  			}, 480*index);
-  		});
+      var imgLoadedCount = 0;
+      $('img').each(function() {
+        var img = new Image();
+        img.onload = function() {
+          imgLoadedCount++;
+          if (imgLoadedCount == $('img').length) {
+            $('.demo td').each(function(index, el) {
+              var $this = $(el);
+              setTimeout(function() {
+                $this.css({
+                  'opacity': '1',
+                  'top': '0'
+                });
+              }, 480*index);
+            });
+          };
+        };
+        img.src = $(this).attr('data-src');
+      });
 	};
 	Template.teamPage.destroyed = function () {
 		$('#bodyItem').removeAttr('style').addClass('isTempNoShow');
